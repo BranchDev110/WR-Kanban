2b4fd0fd9afabcdc44bc38a8331531d9e1d13efe
mac <mac@macs-MacBook-Pro-67.local>
2023-12-18 04:56:59 -0500

    Ready for v0.1

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAeASpBGlrhniVO10ZUsOv6LQQRd-k0epo",
-  authDomain: "stacklok-124cf.firebaseapp.com",
-  projectId: "stacklok-124cf",
-  storageBucket: "stacklok-124cf.appspot.com",
-  messagingSenderId: "317131718077",
-  appId: "1:317131718077:web:2381fbda08693da9b766d4",
-  measurementId: "G-ZQ6GYM09JY"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
